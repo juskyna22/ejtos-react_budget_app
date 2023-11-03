34160b809d6cb993f5c5598bc12c4bcef53b2770
juskyna22 <66147907+juskyna22@users.noreply.github.com>
2023-11-02 21:31:01 -0700

    Update ExpenseList.js

--- src/components/ExpenseList.js
+++ src/components/ExpenseList.js
@@ -4,7 +4,7 @@ import { AppContext } from '../context/AppContext';
 
 const ExpenseList = () => {
     const { expenses } = useContext(AppContext);
-
+/*added a new column here to break allocation into five sections T7 decrease by 10*/
     return (
         <table className='table'>
               <thead className="thead-light">
@@ -12,6 +12,7 @@ const ExpenseList = () => {
               <th scope="col">Department</th>
               <th scope="col">Allocated Budget</th>
               <th scope="col">Increase by 10</th>
+              <th scope="col">Decrease by 10</th>  
               <th scope="col">Delete</th>
             </tr>
           </thead>
