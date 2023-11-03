eec8fb4b600c92b5e12f81d27978fa1718877dd1
juskyna22 <66147907+juskyna22@users.noreply.github.com>
2023-11-03 10:25:11 -0700

    Update AllocationForm.js

--- src/components/AllocationForm.js
+++ src/components/AllocationForm.js
@@ -1,3 +1,6 @@
+/*In AllocationForm.js you will be creating an expense object, containing the name and the cost. This is what will get dispatched as the payload, and 
+what you’ll use to update the state.*/
+
 import React, { useContext, useState } from 'react';
 import { AppContext } from '../context/AppContext';
 
@@ -79,3 +82,4 @@ const AllocationForm = (props) => {
 };
 
 export default AllocationForm;
+/*Here, you are adding form tags, adding a label/input for name, cost and action field, and adding values for various departments.*/
