e080aab3924ffc3314b5e75866685f26870eaa8c
juskyna22 <66147907+juskyna22@users.noreply.github.com>
2023-11-03 10:38:35 -0700

    Update and rename CurrencyType.js to Location.js

---
+++ src/components/Location.js
@@ -0,0 +1,31 @@
+/*In Location.js, you are importing AppContext and adding changeLication class to change the location 
+along with its currencies. 
+When you change the Location, currencies will be updated at all the required places.*/
+
+import React, { useContext } from 'react';
+import { AppContext } from '../context/AppContext';
+
+const Location = () => {
+  const {dispatch } = useContext(AppContext);
+
+    const changeLocation = (val)=>{
+            dispatch({
+                type: 'CHG_LOCATION',
+                payload: val,
+            })
+    }
+  
+    return (
+        <div className='alert alert-secondary'> Location {
+      <select name="Location" id="Location" onChange={event=>changeLocation(event.target.value)}>
+        <option value="$">$ Dollar</option>
+        <option value="£">£ Pound</option>
+        <option value="€">€ Euro</option>
+        <option value="₹">₹ Ruppee</option>
+      </select>	
+      }	
+    </div>
+    );
+};
+
+export default Location;
