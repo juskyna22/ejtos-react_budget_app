4f6fe8ce2eab5d4a4823aa8075e84c6a30a64c9a
juskyna22 <66147907+juskyna22@users.noreply.github.com>
2023-11-05 13:06:46 -0700

    Update Remaining.js

--- src/components/Remaining.js
+++ src/components/Remaining.js
@@ -1,14 +1,16 @@
 import React, { useContext } from 'react';
 import { AppContext } from '../context/AppContext';
+/*Task 5 currency prefix with next line of code, replaced Euro symbol with Location for Task6*/
 const Remaining = () => {
-    const { expenses, budget } = useContext(AppContext);
+    const { expenses, budget, Location } = useContext(AppContext);
     const totalExpenses = expenses.reduce((total, item) => {
         return (total = total + item.cost);
     }, 0);
     const alertType = totalExpenses > budget ? 'alert-danger' : 'alert-success';
     return (
         <div className={`alert ${alertType}`}>
-            <span>Remaining: £{budget - totalExpenses}</span>
+    /*/*Task 5 currency prefix with next line of code, replaced Euro symbol with Location for Task6*/
+            <span>Remaining: {Location}{budget - totalExpenses}</span>
         </div>
     );
 };
